011d70767a3af484a26cd7a37717b5b3918cde3d
sauna1137 <subscription.search@gmail.com>
2021-12-31 01:19:26 +0900

    Error処理を追加

--- SearchGitHubUsersMVP/Model/FetchGitHubUsers.swift
+++ SearchGitHubUsersMVP/Model/FetchGitHubUsers.swift
@@ -13,11 +13,12 @@ class FetchGitHubUsers {
     private let gitHubAPIURL = "https://api.github.com/search/users?q=" // ユーザー情報を取得するAPI q=以下にキーワード
 
     // SearchVCで検索キーワードを元にAPIを叩いて値を返す
-    func fetchGitHubUsers(searchText: String, completion: @escaping ([GitHubUsers]) -> Void) {
+    func fetchGitHubUsers(searchText: String, completion: @escaping ([GitHubUsers]?, String?) -> Void) {
 
         AF.request(gitHubAPIURL + searchText) { _ in } .response { response in
 
             guard let data = response.data else {
+                completion(nil, FetchAPIError.someError.title)
                 return
             }
 
@@ -26,13 +27,15 @@ class FetchGitHubUsers {
                 let totalCount = json.totalCount
 
                 guard totalCount > 0 else {
+                    completion(nil, FetchAPIError.noResult.title)
                     return
                 }
 
-                completion(json.items)
+                completion(json.items, nil)
 
             } catch {
                 print(error.localizedDescription)
+                completion(nil, FetchAPIError.someError.title)
             }
         }
     }
