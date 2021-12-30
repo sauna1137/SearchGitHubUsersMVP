a39d66e3a44380e7c097dd47129ed55ad6fc4561
sauna1137 <subscription.search@gmail.com>
2021-12-31 00:28:31 +0900

    viewController実装

--- SearchGitHubUsersMVP/View/ViewController.swift
+++ SearchGitHubUsersMVP/View/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
+    private var presenter = Presenter()
+    private var gitHubUsers: [GitHubUsers] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        presenter.delegate = self
+
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gitHubUsers.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        cell.imageVIew.sd_setImage(with: URL(string: gitHubUsers[indexPath.row].avatarURL), completed: nil)
+        cell.nameLabel.text = gitHubUsers[indexPath.row].login
+        return cell
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        presenter.searchBarDidSearch(searchText: searchBar.text ?? "")
+    }
+}
+
+extension ViewController: PresenterDelegate {
+    func passFetchedGitHubUsers(githubUsers: [GitHubUsers]) {
+        gitHubUsers = githubUsers
+        tableView.reloadData()
+    }
+}
