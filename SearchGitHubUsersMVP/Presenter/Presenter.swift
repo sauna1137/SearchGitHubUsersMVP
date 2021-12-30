cf9f98f763c86cf069ee2d5ee4417847acd97669
sauna1137 <subscription.search@gmail.com>
2021-12-31 00:27:31 +0900

    Presenter実装

--- SearchGitHubUsersMVP/Presenter/Presenter.swift
+++ SearchGitHubUsersMVP/Presenter/Presenter.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
+protocol PresenterDelegate {
+    func passFetchedGitHubUsers(githubUsers: [GitHubUsers])
+}
 
+class Presenter {
+
+    private var fetchGitHubUsers = FetchGitHubUsers()
+    var delegate: PresenterDelegate?
+
+
+    func searchBarDidSearch(searchText: String) {
+        fetchGitHubUsers.fetchGitHubUsers(searchText: searchText) { gitHubUsers in
+            self.delegate?.passFetchedGitHubUsers(githubUsers: gitHubUsers)
+            print(gitHubUsers)
+        }
+    }
+
+
+}
