198332e8780eccf81f666c1d90bc418936fa5ec6
sauna1137 <subscription.search@gmail.com>
2021-12-30 17:43:37 +0900

    tableViewCell追加

---
+++ SearchGitHubUsersMVP/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  SearchGitHubUsersMVP
+//
+//  Created by KS on 2021/12/30.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+}
