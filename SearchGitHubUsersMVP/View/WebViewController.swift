ae5a7524a5ece4ef649eddb955778af7980c111b
sauna1137 <subscription.search@gmail.com>
2021-12-31 00:28:44 +0900

    webView追加

---
+++ SearchGitHubUsersMVP/View/WebViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WebViewController.swift
+//  SearchGitHubUsersMVP
+//
+//  Created by KS on 2021/12/30.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
